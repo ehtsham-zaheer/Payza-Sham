0c15ec122120c8abcec8a8bb7732e3be387714f3
Ehtsham Zaheer <ehtsham.bsse629@iiu.edu.pk>
2013-06-18 13:05:04 +0500

    Create form.payment.php

---
+++ form.payment.php
@@ -0,0 +1,27 @@
+<!--/*
+* Copyright 2013 Ehtsham Zaheer
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/-->  							
+
+<p class="form-row">
+    <label>Payza Email <span class="required">*</span></label>
+    <input class="input-text" type="text" id="payza_email" name="payza_email"/>
+</p>
+
+<p class="form-row form-row-first">
+	<label>Payza Password <span class="required">*</span></label>
+    <input class="input-text" type="password" id="payza_password" name="payza_password"/>
+</p>
+<div class="clear"></div>
+<img type="image" src="https://www.payza.com/images/payza-buy-now.png" />
